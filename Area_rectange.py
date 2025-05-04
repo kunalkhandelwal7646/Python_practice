b2069907c21fd84a41229f79b8f8f19df31e1c0e
Kunal Khandelwal (Nokia) <kunal.khandelwal@nokia.com>
2025-05-04 04:10:29 +0100

    Area of Rectangle

---
+++ Area_rectange.py
@@ -0,0 +1,8 @@
+# Area of Rectangle
+
+length = float(input("Enter Length of Rectangle - "))
+breadth = float(input("Enter Breadth of Rectangle - "))
+
+area = length * breadth
+
+print(f"Area of Rectangle is {area}")
